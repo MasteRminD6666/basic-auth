45ce9d335ab2003f44958201c93a74a82d3fa70a
MasteRminD6666 <ramikhzaitoun@gmail.com>
2021-11-07 16:38:05 +0200

    adding

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.post('/signup', async (req, res) => {
     const record = await Users.create(req.body);
     res.status(200).json(record);
    
-  } catch (e) { res.status(403).send("Error Creating User" ,);  }
+  } catch (e) { res.status(403).send("Error Creating User");  }
 });
 
 
